71a6569e9302608e5beb7d0f2de320f80502cc4f
ioiotinez <ioiotinez@gmail.com>
2025-04-10 09:31:16 -0300

    init project structure

---
+++ server/src/main.rs
@@ -0,0 +1,11 @@
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::broadcast;
+use std::sync::Arc;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    println!("Server listening on 127.0.0.1:8080");
+
+    
+}
